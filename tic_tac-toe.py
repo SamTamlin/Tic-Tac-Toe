0aa5d55127ebb2616af03f15493d1e6d303ecbec
SamTamlin <113186693+SamTamlin@users.noreply.github.com>
2022-09-22 10:21:53 +0100

    Added a simple AI that takes random moves. Added a Settings & Statistics menu giving the user the ability to change player names and view stats on games played, won, lost and drawn.

--- tic_tac-toe.py
+++ tic_tac-toe.py
@@ -1,10 +1,16 @@
 import os
+from random import randint
 
 
 class new_player:
     def __init__(self, name, side):
         self.name = name
         self.side = side
+        self.human = True
+        self.games_played = 0
+        self.games_won = 0
+        self.games_tied = 0
+        self.games_lost = 0
 
 
 def new_board():
@@ -15,19 +21,77 @@ def new_board():
 def main_menu():
     while True:
         print("\n\tTIC-TAC-TOE")
-        print("\t[1] Start Game")
-        print("\t[Q] Quit")
+        print("\t[1] Single Player Game")
+        print("\t[2] Multiplayer Game")
+        print("\t[S] Settings & Statistics")
+        print("\t[Q] Quit Game")
 
         player_choice = input("\t Please make your selection: ").lower()
 
         if player_choice == '1':
+            player_2.human = False
             return True
+
+        elif player_choice == '2':
+            player_2.human = True
+            return True
+
+        elif player_choice == 's':
+            settings_menu()
 
         elif player_choice == 'q':
             return False
 
         else:
-            print("\t Sorry I didn't understand that.")
+            print("\tSorry I didn't understand that.")
+
+
+def settings_menu():
+    os.system('cls')
+    while True:
+        print("\n\tSettings & Statistics Menu")
+        print("\t[1] Rename %s" % player_1.name.title())
+        print("\t[2] Rename %s" % player_2.name.title())
+        print("\t[S] Statistics")
+        print("\t[Q] Exit Settings Menu")
+
+        settings_choice = input("\tPlease make your selection: ").lower()
+
+        if settings_choice == '1':
+            rename(player_1)
+
+        elif settings_choice == '2':
+            rename(player_2)
+
+        elif settings_choice == 's':
+            stats()
+
+        elif settings_choice == 'q':
+            return False
+
+        else:
+            print("\tSorry I didn't understand that.")
+
+
+def stats():
+    os.system('cls')
+    print("\n\tStatistics")
+    print("\n\t'%s' has played %d games: \n\t\tWon: %s \n\t\tLost: %s "
+          "\n\t\tDrawn: %s" % (player_1.name.upper(), player_1.games_played,
+                               player_1.games_won, player_1.games_lost,
+                               player_1.games_tied))
+    print("\n\t'%s' has played %d games: \n\t\tWon: %s \n\t\tLost: %s "
+          "\n\t\tDrawn: %s" % (player_2.name.upper(), player_2.games_played,
+                               player_2.games_won, player_2.games_lost,
+                               player_2.games_tied))
+    input("\n\t Press enter to return to the Settings menu.")
+
+def rename(player):
+    new_name = input(
+        "\t%s, what would you like to be called? "
+        % player.name.title()).lower()
+    if new_name:
+        player.name = new_name
 
 
 def print_board(board):
@@ -41,27 +105,45 @@ def print_board(board):
     print("\n\ty")
 
 
-def get_move(name):
+def get_move(turn_board, player):
     """Takes the name of the player and asks them for co-ordinates. Checks
     that the co-ordinates are reasonable and requests again if they are not.
     Function then returns the co-ordinates as integers."""
-    while True:
-        y = input("\n\t%s enter your Y co-ordinate: " % name.title())
-        # If the input is not a co-ordinate, print an error and ask again.
-        if y != '0' and y != '1' and y != '2':
-            print("\tYou must enter a number between 0 and 2.")
-        else:
-            break
+    if player.human:
+        while True:
+            y = input("\n\t%s enter your Y co-ordinate: "
+                      % player.name.title())
+            # If the input is not a co-ordinate, print an error and ask again.
+            if y != '0' and y != '1' and y != '2':
+                print("\tYou must enter a number between 0 and 2.")
+            else:
+                break
 
-    while True:
-        x = input("\t%s enter your X co-ordinate: " % name.title())
-        # If the input is not a co-ordinate, print an error and ask again.
-        if x != '0' and x != '1' and x != '2':
-            print("\tYou must enter a number between 0 and 2.")
-        else:
-            break
+        while True:
+            x = input("\t%s enter your X co-ordinate: " % player.name.title())
+            # If the input is not a co-ordinate, print an error and ask again.
+            if x != '0' and x != '1' and x != '2':
+                print("\tYou must enter a number between 0 and 2.")
+            else:
+                break
+    else:
+        y, x = get_random_move(turn_board, player)
 
     return int(y), int(x)
+
+
+def get_random_move(turn_board, player):
+    """Takes a board and a player, then returns co-ordinates
+    of a random empty cell."""
+    good_move = False
+    while not good_move:
+        y = randint(0, 2)
+        x = randint(0, 2)
+        cords = y, x
+
+        good_move = is_valid_move(turn_board, cords)
+
+    return y, x
 
 
 def is_valid_move(board, cords):
@@ -82,7 +164,7 @@ def make_move(board, move_cords, player):
     return next_board
 
 
-def take_turn(turn_board, player):
+def take_turn(turn_board, turn_taker, other_player):
     """Takes a board and player which are used to process a players turn.
     Then returns an updated board and if the game should continue by returning
     True or False."""
@@ -91,7 +173,7 @@ def take_turn(turn_board, player):
 
     checking = True
     while checking:
-        coordinates = get_move(player.name)
+        coordinates = get_move(turn_board, turn_taker)
         valid = is_valid_move(turn_board, coordinates)
         if valid:
             checking = False
@@ -100,20 +182,28 @@ def take_turn(turn_board, player):
             print_board(turn_board)
             print("\n\tSorry, this move has already been made!!.")
 
-    turn_board = make_move(turn_board, coordinates, player)
+    turn_board = make_move(turn_board, coordinates, turn_taker)
 
     winner = get_winner(turn_board)
     drawn = check_for_draw(turn_board)
 
     if winner is not None:
         print_board(turn_board)
-        print("\n\tCONGRATULATIONS %s(%s) is the winner!"
-              % player.name.title(), player.side)
+        print("\n\t'%s' WINS! Congratulations %s you are the winner!!!"
+              % (turn_taker.side, turn_taker.name.title()))
+        turn_taker.games_won += 1
+        turn_taker.games_played += 1
+        other_player.games_played += 1
+        other_player.games_lost += 1
         return turn_board, False
 
     if drawn:
         print_board(turn_board)
         print("\n\tTHE GAME IS A TIE!")
+        turn_taker.games_tied += 1
+        turn_taker.games_played += 1
+        other_player.games_tied += 1
+        other_player.games_played += 1
         return turn_board, False
 
     return turn_board, True
@@ -171,11 +261,12 @@ while True:
         break
 
     game_board = new_board()
-
     while playing_game:
         # Player_1's turn
-        game_board, playing_game = take_turn(game_board, player_1)
+        game_board, playing_game = take_turn(
+            game_board, player_1, player_2)
 
         if playing_game:
             # Player_2'a turn.
-            game_board, playing_game = take_turn(game_board, player_2)
+            game_board, playing_game = take_turn(
+                game_board, player_2, player_1)
